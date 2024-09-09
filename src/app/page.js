c60d0076e70ff5a85113ac89959912fc85e9175f
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-09 23:19:42 +0200

    Fetch movie data from tmdb.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
 import Image from "next/image";
+import {searchParams} from "next/navigation";
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+  
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated":"trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:1000}}
+  );
+  if(!response.ok){
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+  const results = data.results;
+  
   return (
-    <div className="text-red-500">Home</div>
+    <div>
+      <Results results={results} />
+    </div>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((results) => (
+        <div key={results.id}>{results.original_title}</div>
+      ))}
+    </div>
+  );
+}
