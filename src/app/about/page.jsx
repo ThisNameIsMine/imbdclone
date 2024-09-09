c001e36ff68140e55227b63899dade89607e0b48
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-09 22:33:18 +0200

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+      <p>
+        This is a clone of the IMDb website. It is built with Next.js and
+        Tailwind CSS.
+      </p>
+      <p>
+        The IMDb website is a popular website for finding information about
+        movies and TV shows. It provides information about the latest movies and
+        TV shows, as well as information about actors, directors, and other
+        people involved in the film industry.
+      </p>
+      <p>
+        This clone website is built using Next.js, a popular React framework for
+        building web applications. Next.js provides a number of features that
+        make it easy to build fast and responsive web applications, including
+        server-side rendering, static site generation, and automatic code
+        splitting.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eaque,
+        distinctio voluptate, adipisci non nisi ea explicabo nihil sit quasi
+        exercitationem sunt. Quo libero numquam, ullam rerum nostrum nulla
+        possimus!
+      </p>
+    </div>
+  );
+};
+
+export default About;
